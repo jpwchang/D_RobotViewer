3af4c786f0150d52cfde912481a5ad160e04984a
Jonathan Chang <jonathan.chang13@gmail.com>
2013-03-07 19:41:57 -0800

    First commit

---
+++ Assets/RobotScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+
+public class RobotScript : MonoBehaviour {
+    private Vector3 rotation;
+	// Use this for initialization
+	void Start () {
+        rotation = new Vector3(0.0f, 0.1f, 0.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(rotation);
+	}
+}
